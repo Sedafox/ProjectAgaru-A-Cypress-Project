a7d504a1cb34f919b4344517a81fba72bce2ca75
sedafox <usagimadokii@gmail.com>
2022-06-21 14:03:17 -0500

    comments

--- cypress/e2e/basicAuth.cy.js
+++ cypress/e2e/basicAuth.cy.js
@@ -4,8 +4,8 @@ const base = new Base()
 const basicAuthPage = new BasicAuthPage()
 
 describe('Tests the core functionality of the Basic Auth page', () =>{
-    it('Successfully login by appending username and password in URL', function() {
-        cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
-        cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
+    it('Successfully login by appending username and password in URL', () => {
+        cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth') //the admin:admin at the start indicates username:password
+        cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.') //If we find this then we logged in!
     })
 })
