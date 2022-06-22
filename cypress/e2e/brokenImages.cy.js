7081f8767751eca8b73792fb1732ffa40ac11da9
sedafox <usagimadokii@gmail.com>
2022-06-22 08:56:57 -0500

    Implemented my own code to find broken images.

--- cypress/e2e/brokenImages.cy.js
+++ cypress/e2e/brokenImages.cy.js
@@ -7,11 +7,13 @@ describe('Tests designed to find Broken Images', () => {
     it('Goes to the Broken Images Page', () =>{
         base.visitBrokenImagesTesting()
         cy.get('img')
-            .should('be.visible')
+            .should('be.visible')//Image should be visible
             .and(($img) => {
                 //naturalWidth & Height are set when image loads
-                for(let i in $img){
-                    expect($img[i].naturalWidth).to.be.greaterThan(-1)
+                for(let i in $img) { //for every image found...
+                    if(i <= $img.length) { //For reasons unbeknownst to man, if I don't have this if statement then the system tries to scan for an extra image. Why.
+                        expect($img[i].naturalWidth).to.be.greaterThan(0) //Expect all images to have a natural width > 0, which indicates that the image has loaded
+                    }
                 }
             })
 
