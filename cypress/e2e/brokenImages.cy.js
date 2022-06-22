b8d8f0d91c94a8a8ec7d0e476a3a52d75af75598
sedafox <usagimadokii@gmail.com>
2022-06-22 08:45:31 -0500

    Found simpler code, but it only works. Kinda.

--- cypress/e2e/brokenImages.cy.js
+++ cypress/e2e/brokenImages.cy.js
@@ -7,8 +7,13 @@ describe('Tests designed to find Broken Images', () => {
     it('Goes to the Broken Images Page', () =>{
         base.visitBrokenImagesTesting()
         cy.get('img')
-            .filter(':visible')
-            .should(($imgs) => $imgs.map((i, /** @type {HTMLImageElement} */ img) => expect(img.naturalWidth).to.be.greaterThan(0)));
-            //TODO figure out what the heck the above code is... It works... but how?
+            .should('be.visible')
+            .and(($img) => {
+                //naturalWidth & Height are set when image loads
+                for(let i in $img){
+                    expect($img[i].naturalWidth).to.be.greaterThan(-1)
+                }
+            })
+
     })
 })
