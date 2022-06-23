e88eeb53b3f985ad0e8bb87f95fc4d099c7be2d2
SedaFox <Usagimadokii@gmail.com>
2022-06-23 17:47:41 -0500

    Update addRemoveElementsPage.js

--- cypress/Integration/PageObject/addRemoveElementsPage.js
+++ cypress/Integration/PageObject/addRemoveElementsPage.js
@@ -2,7 +2,7 @@ import Base from "./base";
 const base = new Base()
 
 class AddRmElementsPage{
-    addElementButton() {return cy.get('button[onclick="addElement()"]') }
-    deleteButtons() {return cy.get('button[class="added-manually"]')}
+    addElementButton() { return cy.get('button[onclick="addElement()"]') }
+    deleteButtons() { return cy.get('button[class="added-manually"]') }
 }
 export default AddRmElementsPage
