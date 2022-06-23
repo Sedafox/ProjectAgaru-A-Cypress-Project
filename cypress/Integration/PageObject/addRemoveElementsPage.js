f4da3a86220bb7c32b0c385e2b2041cea8c5eaad
sedafox <usagimadokii@gmail.com>
2022-06-23 17:37:43 -0500

    Based on Feedback, Ensured that on PageObject it only describes the objects on the page in the class. And modified all the testing functionality to be done on the e2e test. This way if a selector changes, I only have to change it on one place and it doesn't force me to rewrite my class code. The test code only changes when functionality changes.

--- cypress/Integration/PageObject/addRemoveElementsPage.js
+++ cypress/Integration/PageObject/addRemoveElementsPage.js
@@ -2,12 +2,7 @@ import Base from "./base";
 const base = new Base()
 
 class AddRmElementsPage{
-    numberOfExpectedDeleteButtons(expectedNum){
-       return cy.get('button[class="added-manually"]').then((numOfDeleteButtons) => { //Delete buttons have the added-manually class
-           if(numOfDeleteButtons.length != expectedNum){ //If there are more or less Delete Buttons then expected...
-               reportError("There are More or Less Delete Buttons Than Expected") //Error!
-           }
-       })
-    }
+    addElementButton() {return cy.get('button[onclick="addElement()"]') }
+    deleteButtons() {return cy.get('button[class="added-manually"]')}
 }
 export default AddRmElementsPage
--- cypress/e2e/addRemoveElements.cy.js
+++ cypress/e2e/addRemoveElements.cy.js
@@ -9,32 +9,26 @@ describe('Tests the Add and Remove Elements Page', () => {
         base.visitAddRemoveTesting()
     })
     it('Should only have An Add Element Button on first visit', () => {
-        cy.get('button[onclick="addElement()"]')
+        addRemoveElementPage.addElementButton().eq(0).should('be.visible');
+        addRemoveElementPage.addElementButton().eq(1).should('not.exist') //There should only be 1 addElement Button
     })
-    it('Should add a Button if I click Add Element', () => {
-        cy.get('button[onclick="addElement()"]')
-            .click()
-        addRemoveElementPage.numberOfExpectedDeleteButtons(1) //We are expecting 1 Delete button to be on the page at this point
+    it('Should add only 1 Button if I click Add Element', () => {
+        addRemoveElementPage.addElementButton().click();
+        addRemoveElementPage.deleteButtons().eq(0).should('be.visible')
+        addRemoveElementPage.deleteButtons().eq(1).should('not.exist')
     })
-    it('Should add an element I click each time. Random from 1-20', () => {
-        let previousAmount = Cypress.$('button').length
-        for (let i = 0; i < Math.random() * 20; i++) { //Click the Add Element button a random number of times
-            cy.get('button').contains('Add Element')
-                .click()
-            addRemoveElementPage.numberOfExpectedDeleteButtons(previousAmount)
-            previousAmount++ //We expect another button to display after each click
+    it('Should be able to click the Add Element multiple times', () => {
+        for (let i = 0; i < Math.random() * 20; i++) { //Randomly run the below code from 1-20 times
+            addRemoveElementPage.addElementButton().click()
+            addRemoveElementPage.deleteButtons().eq(i).should('be.visible')
+            addRemoveElementPage.deleteButtons().eq(i + 1).should('not.exist')
+
         }
     })
-    it('Should remove a button if I click delete. Delete all buttons.', () => {
-        //Find How many Buttons there are
-        let OriginalAmount = Cypress.$('button').length
-        let newAmount = OriginalAmount
-        for (let i = 0; OriginalAmount - 1 > i; i++) { //Click the Delete button however many times there are delete buttons
-            cy.get('Button').contains('Delete')
-                .click()
-            addRemoveElementPage.numberOfExpectedDeleteButtons(newAmount) 
-            newAmount-- //We are expecting 1 less Delete button every time we click
-        }
-        //For each button, click delete button - 1
+    it('Should remove a button if I click delete.', () => {
+        addRemoveElementPage.addElementButton().click()
+        addRemoveElementPage.deleteButtons().eq(0).should('be.visible')
+        addRemoveElementPage.deleteButtons().click()
+        addRemoveElementPage.deleteButtons().should('not.exist') //Delete button shouldn't be there anymore
     })
 })
