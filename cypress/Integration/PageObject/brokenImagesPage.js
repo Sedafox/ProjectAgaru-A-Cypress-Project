b2f33db582e4c7c9d76bb6e3124e3e4630c77945
sedafox <usagimadokii@gmail.com>
2022-06-23 17:45:48 -0500

    POM shenanigans

--- cypress/Integration/PageObject/brokenImagesPage.js
+++ cypress/Integration/PageObject/brokenImagesPage.js
@@ -1,4 +1,4 @@
 class BrokenImagesPage{
-
+    images() { return cy.get('div > img') }
 }
 export default BrokenImagesPage
--- cypress/e2e/brokenImages.cy.js
+++ cypress/e2e/brokenImages.cy.js
@@ -3,19 +3,17 @@ import BrokenImagesPage from "../Integration/PageObject/brokenImagesPage";
 const base = new Base()
 const brokenImagesPage = new BrokenImagesPage()
 
+
+//This test is designed to fail. Two of the images will fail to load, thus making their natural width less than 1. A website owner may want to know when their images don't load
 describe('Tests designed to find Broken Images', () => {
-    it('Goes to the Broken Images Page', () =>{
+    it('Webpage should load all images in div', () =>{
         base.visitBrokenImagesTesting()
-        cy.get('div > img') //get the three images in the div
-            .should('be.visible')//Images should be visible
+        brokenImagesPage.images().should('be.visible')
             .and(($img) => {
-                //naturalWidth & Height are set when image loads
                 for(let i in $img) { //for every image found...
-                    if(i <= $img.length) { 
-                        expect($img[i].naturalWidth).to.be.greaterThan(0) //Expect all images to have a natural width > 0, which indicates that the image has loaded
-                    }
+                    if(i <= $img.length) { expect($img[i].naturalWidth).to.be.greaterThan(0) }
+                    //The above code first checks if the images are visible, then for every image found, check the naturalWidth. If greater than 0, the image loaded successfully
                 }
             })
-
     })
 })
