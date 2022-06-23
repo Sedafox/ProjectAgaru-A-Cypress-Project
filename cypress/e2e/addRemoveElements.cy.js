6a49a6e3674dd1f38d492505c39443ddb52e5a92
sedafox <usagimadokii@gmail.com>
2022-06-23 15:40:48 -0500

    formatting

--- cypress/e2e/addRemoveElements.cy.js
+++ cypress/e2e/addRemoveElements.cy.js
@@ -8,28 +8,28 @@ describe('Tests the Add and Remove Elements Page', () => {
     beforeEach('Visit the Elements Page', () => {
         base.visitAddRemoveTesting()
     })
-    it('Should only have An Add Element Button on first visit', () =>{
+    it('Should only have An Add Element Button on first visit', () => {
         cy.get('button[onclick="addElement()"]')
     })
-    it('Should add a Button if I click Add Element', () =>{
+    it('Should add a Button if I click Add Element', () => {
         cy.get('button[onclick="addElement()"]')
             .click()
         addRemoveElementPage.numberOfExpectedDeleteButtons(1) //We are expecting 2 buttons to be on the page at this point
     })
-    it('Should add an element I click each time. Random from 1-20', ()=>{
+    it('Should add an element I click each time. Random from 1-20', () => {
         let previousAmount = Cypress.$('button').length
-        for(let i = 0; i < Math.random() * 20; i++) { //Click the Add Element button a random number of times
+        for (let i = 0; i < Math.random() * 20; i++) { //Click the Add Element button a random number of times
             cy.get('button').contains('Add Element')
                 .click()
             addRemoveElementPage.numberOfExpectedDeleteButtons(previousAmount) //We expect another button to display after each click
             previousAmount++
         }
-        })
+    })
     it('Should remove a button if I click delete. Delete all buttons.', () => {
         //Find How many Buttons there are
         let OriginalAmount = Cypress.$('button').length
         let newAmount = OriginalAmount
-        for(let i = 0; OriginalAmount - 1 > i; i++){ //Click the Delete button however many times there are delete buttons
+        for (let i = 0; OriginalAmount - 1 > i; i++) { //Click the Delete button however many times there are delete buttons
             cy.get('Button').contains('Delete')
                 .click()
             addRemoveElementPage.numberOfExpectedDeleteButtons(newAmount) //We are expecting 1 less button every time we click
