dd51f930f0a050599bb3038cf3796c0b59cb3fd0
sedafox <usagimadokii@gmail.com>
2022-06-24 17:08:02 -0500

    Revised Test

--- cypress/e2e/checkBoxes.cy.js
+++ cypress/e2e/checkBoxes.cy.js
@@ -10,18 +10,15 @@ describe('Testing Checkboxes', () => {
         checkBoxesPage.checkBox().eq(1).should('be.checked')
     })
     it('Should ensure that clicking a checkbox checks it', () => {
-        checkBoxesPage.checkBox().eq(0).should('not.be.checked')
-            .click()
+        checkBoxesPage.checkBox().eq(0).click()
         checkBoxesPage.checkBox().eq(0).should('be.checked')
     })
     it('Should ensure that clicking a checkbox that is already checked will uncheck it', () => {
-        checkBoxesPage.checkBox().eq(1).should('be.checked')
-            .click()
+        checkBoxesPage.checkBox().eq(1).click()
         checkBoxesPage.checkBox().eq(1).should('not.be.checked')
     })
     it('Should ensure that clicking a checkbox will allow the user to uncheck it', () => {
-        checkBoxesPage.checkBox().eq(0).should('not.be.checked')
-            .click()
+        checkBoxesPage.checkBox().eq(0).click()
         checkBoxesPage.checkBox().eq(0).should('be.checked')
             .click()
         checkBoxesPage.checkBox().eq(0).should('not.be.checked')
