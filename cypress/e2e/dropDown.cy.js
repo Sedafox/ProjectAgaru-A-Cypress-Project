d744fa2bfb1b33e0b80cfb2dc7b42bfe7a884a98
jeffery <usagimadokii@gmail.com>
2022-06-28 06:57:58 -0500

    Changed describe title

--- cypress/e2e/dropDown.cy.js
+++ cypress/e2e/dropDown.cy.js
@@ -3,7 +3,7 @@ import DropDownPage from "../Integration/PageObject/dropDownPage";
 const base = new Base()
 const dropDownPage = new DropDownPage()
 
-describe('Does something', () =>{
+describe('Selections Options from a Dropdown Menu', () =>{
     beforeEach('Refreshes Page', () =>{ base.visitDropdownTesting() })
     it('Selects Option 1', () => { dropDownPage.dropDownMenu().select('Option 1') })
     it('Selects Option 2', () => { dropDownPage.dropDownMenu().select('Option 2') })
