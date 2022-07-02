9542a06ec37b12d46a50945f25258ec1917b4bd8
jeffery <usagimadokii@gmail.com>
2022-07-02 15:56:56 -0500

    Added File Floating Menu capabilities

--- cypress/e2e/floatingMenu.cy.js
+++ cypress/e2e/floatingMenu.cy.js
@@ -10,12 +10,12 @@ describe('Tests things related to a floating menu', () => {
     })
 
     it('scrolls the page down, then clicks the home button', () => {
-        cy.scrollTo(0,500)
+        cy.scrollTo(0,500) //scrolldown some
         floatingMenuPage.floatingHomeButton().click({scrollBehavior: false})
     })
 
     it('scrolls the page down, then clicks the News button', () => {
-        cy.scrollTo(0,500)
+        cy.scrollTo(0,500) //scrolldown some
         floatingMenuPage.floatingNewsButton().click({scrollBehavior: false})
         //We want to tell Cypress not to scroll, this makes it so if the floating menu wasn't on screen,it wouldn't scroll all the way up to click it
     })
