eeb80c98c42538c2ad472590981cc1b86261d19a
jeffery <usagimadokii@gmail.com>
2022-07-02 14:39:28 -0500

    File Download functionality

---
+++ cypress/Integration/PageObject/fileDownloadPage.js
@@ -0,0 +1,24 @@
+class FileDownloadPage {
+    someFileTxt() { return cy.get('.example > a') }
+    downloadFile(cssSelector){
+        /*
+        Workaround for Cypress expecting a page to load after download
+        https://github.com/cypress-io/cypress-example-kitchensink/issues/453
+         */
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+                setTimeout(function () { doc.location.reload() }, 500)
+            })
+
+            /* Make sure the file exists */
+            cy.intercept('/', (req) => {
+                req.reply((res) => {
+                    expect(res.statusCode).to.equal(200);
+                });
+            });
+
+            cy.get(`${cssSelector}`).click() //Clicks the cssSelector passed in
+        })
+    }
+}
+export default FileDownloadPage
