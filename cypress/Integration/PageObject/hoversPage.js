f4c929ed7170579b825a76aeb3439ea2fab5b704
jeffery <usagimadokii@gmail.com>
2022-07-03 08:47:35 -0500

    Added comment

--- cypress/Integration/PageObject/hoversPage.js
+++ cypress/Integration/PageObject/hoversPage.js
@@ -1,7 +1,7 @@
 class HoversPage{
     firstImage() { return cy.get(':nth-child(3) > img') }
-    hiddenUserName(username) { return cy.contains(`${username}`) }
-    viewProfile(userNumber) { return cy.get(`a[href="/users/${userNumber}"`) }
+    hiddenUserName(username) { return cy.contains(`${username}`) } //Pass in a username when calling this function
+    viewProfile(userNumber) { return cy.get(`a[href="/users/${userNumber}"`) } //Pass in the usernumber (1-3) when calling this function
     secondImage() { return cy.get(':nth-child(4) > img') }
     thirdImage() { return cy.get(':nth-child(5) > img') }
 }
