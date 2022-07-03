a6cb49b0d9353a798635db173fe5afc52e75891b
jeffery <usagimadokii@gmail.com>
2022-07-03 11:32:07 -0500

    Made Electron location random

--- cypress/e2e/geoLocation.cy.js
+++ cypress/e2e/geoLocation.cy.js
@@ -8,7 +8,7 @@ describe('Tests GeoLocationPage', () => {
     beforeEach('refreshes page', () => {
         //Electron on Mac unfortunately can not call on a location. So we are going to use a helper function to set a fake location before the page loads
         //Is Electron the browser? If so, call fakelocation() from geoLocationPage, pass some co-ords, then hand it off to the visit function in base
-        Cypress.isBrowser('Electron') ? base.visitGeolocationTestingElectron(geoLocationPage.fakeLocation('48', '2')) : base.visitGeolocationTesting()
+        Cypress.isBrowser('Electron') ? base.visitGeolocationTestingElectron(geoLocationPage.fakeLocation(Math.random() * 90, Math.random() * 180)) : base.visitGeolocationTesting()
     })
     it('Should say "Click the button to get your current latitudeText and longitudeText"', () => {
         geoLocationPage.findPageText().should('be.visible')
