8551c0eb0009aafb16a1ac283410cf12a1198fa1
jeffery <usagimadokii@gmail.com>
2022-07-03 07:16:19 -0500

    Added comment for clarity

--- cypress/Integration/PageObject/challengingDOMPage.js
+++ cypress/Integration/PageObject/challengingDOMPage.js
@@ -2,7 +2,7 @@ class ChallengingDOMPage{
     buttonsDiv(){ return cy.get('.large-2.columns') }
     table(){ return cy.get('table') }
     canvas(){ return cy.get('#canvas') }
-    fourthEditButton(){ return cy.get(':nth-child(4) > :nth-child(7) > [href="#edit"]')}
+    fourthEditButton(){ return cy.get(':nth-child(4) > :nth-child(7) > [href="#edit"]')} // 4th row, seventh column, the edit button
     deleteButton(){ return cy.get('a[href="#delete"]') }
 }
 export default ChallengingDOMPage
