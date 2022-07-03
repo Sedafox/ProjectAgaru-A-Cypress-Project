7b2c6544ce4c8a8b5a65c6dce4529804c4c5b132
jeffery <usagimadokii@gmail.com>
2022-07-03 08:44:40 -0500

    added comment

--- cypress/e2e/noABTesting.js
+++ cypress/e2e/noABTesting.js
@@ -4,6 +4,7 @@ import Base from "../Integration/PageObject/base"
 const noABTestingPage = noABTestingPage()
 const base = new Base()
 
+//I think this page might be a little broken. I see the flashing images in the div, but nothing is actually happening. Can't trigger anything. Hmmm...
 describe('ABTestingPage', () =>{
     it('Goes to the ABTestingPage', () =>{
         base.visitABTesting()
