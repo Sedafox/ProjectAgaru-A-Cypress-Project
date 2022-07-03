0335272231b0efd7bd0a7b8311c459f3c70a99da
jeffery <usagimadokii@gmail.com>
2022-07-03 12:05:55 -0500

    All browsers now use fake location so that running tests in CLI pass

--- cypress/e2e/geoLocation.cy.js
+++ cypress/e2e/geoLocation.cy.js
@@ -6,16 +6,15 @@ const geoLocationPage = new GeoLocationPage()
 
 describe('Tests GeoLocationPage', () => {
     beforeEach('refreshes page', () => {
-        //Electron on Mac unfortunately can not call on a location. So we are going to use a helper function to set a fake location before the page loads
-        //Is Electron the browser? If so, call fakelocation() from geoLocationPage, pass some co-ords, then hand it off to the visit function in base
-        Cypress.isBrowser('Electron') ? base.visitGeolocationTestingElectron(geoLocationPage.fakeLocation(Math.random() * 90, Math.random() * 180)) : base.visitGeolocationTesting()
+        //When running CLI the chrome browser/electron browser cant automatically agree to share location. So we are going to set up a fake location
+        //call fakelocation() from geoLocationPage, pass some co-ords, then hand it off to the visit function in base
+        base.visitGeolocationTestingElectron(geoLocationPage.fakeLocation(Math.random() * 90, Math.random() * 180))
     })
     it('Should say "Click the button to get your current latitudeText and longitudeText"', () => {
         geoLocationPage.findPageText().should('be.visible')
     })
     it('Should show current Latitude and Longitude after clicking the button', () => {
             geoLocationPage.findMyLocationBttn().click()
-            if(!Cypress.isBrowser('Electron')){ cy.wait(6000) /*If the browser isn't electron, we need to wait or else the page will time out. */ }
             geoLocationPage.latitudeText().should('be.visible')
             geoLocationPage.longitudeText().should('be.visible')
     })
