713fa82ec17c7991e21c2e07caaa61f7a6ff6941
jeffery <usagimadokii@gmail.com>
2022-07-03 07:44:03 -0500

    changed should('exit') to should('be.visible'). Need to figure out how to reliably test elements that shows up randomly.

---
+++ cypress/e2e/TODO/disappearingElements.cy.js
@@ -0,0 +1,15 @@
+import Base from "../../Integration/PageObject/base"
+import DisappearingElementsPage from "../../Integration/PageObject/disappearingElementsPage";
+const base = new Base()
+const disappearingElementsPage = new DisappearingElementsPage()
+
+describe('This page is designed to test disappearing elements', () => {
+    beforeEach('', () => { base.visitDisappearingElmntsTesting()} )
+    it('Home Button should exist', () => { disappearingElementsPage.homeButton().should('be.visible') })
+    it('About Button Should Exist', () => { disappearingElementsPage.aboutButton().should('be.visible') })
+    it('Contact Button Should Exist', () => { disappearingElementsPage.contactButton().should('be.visible') })
+    it('Portfolio Button Should Exist', () => { disappearingElementsPage.portfolioButton().should('be.visible') })
+    // Gallery Button only shows up on a 50/50 chance, so it's not always expected to be there
+    // it('Gallery Button Should Exist', () => { disappearingElementsPage.galleryButton().should('exist') })
+})
+
