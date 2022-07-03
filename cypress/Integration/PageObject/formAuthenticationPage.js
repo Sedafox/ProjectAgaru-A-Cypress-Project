fb098cb29e59d1dfaba0dca65cbe26942dae7e91
jeffery <usagimadokii@gmail.com>
2022-07-02 22:05:26 -0500

    Added Form Authentication functionality

---
+++ cypress/Integration/PageObject/formAuthenticationPage.js
@@ -0,0 +1,17 @@
+class FormAuthenticationPage {
+    userNameField() { return cy.get('#username') }
+    passwordField() { return cy.get('#password') }
+    submitButton() { return cy.get('.radius')}
+    logOutButton() { return cy.get('.button.secondary.radius')}
+
+    enterCorrectInfo() { //Log in successfully
+        this.userNameField().type('tomsmith')
+        this.passwordField().type('SuperSecretPassword!')
+    }
+
+    enterInvalidInfo() { //Log in unsuccessfully
+        this.userNameField().type('whodathunk')
+        this.passwordField().type('That I\'d be coding some day')
+    }
+}
+export default FormAuthenticationPage
---
+++ cypress/e2e/formAuthentication.cy.js
@@ -0,0 +1,29 @@
+import Base from "../Integration/PageObject/base"
+import FormAuthenticationPage from "../Integration/PageObject/formAuthenticationPage";
+
+const base = new Base()
+const formAuthenticationPage = new FormAuthenticationPage()
+
+describe('Tests Form Authentication', () => {
+
+    beforeEach('Refresh the page', () => {
+        base.visitFormAuthenticationTesting()
+    })
+
+    it('Logs in Successfully', () => {
+        formAuthenticationPage.enterCorrectInfo() //Does cy.get on username field and enters username. Does cy.get on password field and enters password
+        formAuthenticationPage.submitButton().click()
+        //Verify we logged in
+        cy.contains('Welcome to the Secure Area')
+        cy.contains('Logout')
+    })
+    it('Logs out after logging in', () => {
+        formAuthenticationPage.enterCorrectInfo()
+        formAuthenticationPage.logOutButton().click()
+    })
+    it('Enters the incorrect username and password', () => {
+        formAuthenticationPage.enterInvalidInfo()
+        //Verify we failed to log in
+        cy.contains('Your username is invalid!')
+    })
+})
