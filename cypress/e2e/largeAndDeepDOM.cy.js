03a873aefc34d93ef85f350927064822ea4cde00
jeffery <usagimadokii@gmail.com>
2022-07-04 11:10:36 -0500

    TODO

---
+++ cypress/e2e/largeAndDeepDOM.cy.js
@@ -0,0 +1,12 @@
+import Base from "../Integration/PageObject/base"
+import LargeAndDeepDOMPage from "../TODO/largeAndDeepDOMPage";
+
+const base = new Base()
+const largeAndDeepDOMPage = new LargeAndDeepDOMPage()
+
+describe('It tests a very large and deep DOM in a webpage', () => {
+    beforeEach('Refresh page', () => { base.visitLargeNDeepDOMTesting() })
+    it('Finds 35.1 in the Div section', () => {
+        cy.get('h4').contains('35.1')
+    })
+})
