ff48cb17b6182675ef03c5ec58fa780f9dfeff18
sedafox <usagimadokii@gmail.com>
2022-07-06 21:32:23 -0500

    Firefox has a different error than other browsers and will fail. Updated code to fix this

--- cypress/e2e/javaScriptOnLoadError.cy.js
+++ cypress/e2e/javaScriptOnLoadError.cy.js
@@ -4,10 +4,17 @@ import JavaScriptOnLoadErrorPage from "../Integration/PageObject/javaScriptOnLoa
 const base = new Base()
 const javaScriptOnLoadErrorPage = new JavaScriptOnLoadErrorPage()
 
-Cypress.on('uncaught:exception', (err, runnable) => { //If there is an uncaught exception
-    expect(err.message).to.contain("Cannot read properties of undefined (reading 'xyz')") //If the error contains "reading 'xyz'" (which this page's error message does)
-    return false //Pass the test anyway
-})
+if (Cypress.isBrowser('Firefox')) { //If the browser is firefox
+        Cypress.on('uncaught:exception', (err, runnable) => { //If there is an uncaught exception
+            expect(err.message).to.contain("document.propertyThatDoesNotExist") //If the error contains document.propertyThatDoesNotExist
+            return false //Pass the test anyway
+        })
+} else { //If the browser is not firefox (because firefox has a different error for some reason)
+    Cypress.on('uncaught:exception', (err, runnable) => { //If there is an uncaught exception
+        expect(err.message).to.contain("Cannot read properties of undefined (reading 'xyz')") //If the error contains "reading 'xyz'" (which this page's error message does)
+        return false //Pass the test anyway
+    })
+}
 
 describe('Tests Getting a Java Script error on load', () => {
     it('Tests the onLoad error', () => { base.visitJavascriptonloadTesting() }) //Without the above code, this would fail because of the javascript error.
