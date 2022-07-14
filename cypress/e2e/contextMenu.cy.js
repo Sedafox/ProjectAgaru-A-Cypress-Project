008ce568a39517658543c33f064cee9047f9c15c
jeffery <usagimadokii@gmail.com>
2022-07-14 09:11:55 -0500

    Added assertion and note

--- cypress/e2e/contextMenu.cy.js
+++ cypress/e2e/contextMenu.cy.js
@@ -5,6 +5,7 @@ const contextMenuPage = new ContextMenuPage()
 describe('Right clicks a Context Menu', () => {
     beforeEach('', () => { contextMenuPage.visitContextMenuTesting() })
     it('Should right click the Context Menu', () => {
-        contextMenuPage.contextMenu().rightclick()
+        contextMenuPage.contextMenu().rightclick() //Right clicking the context menu opens a Javascript alert. We check for that below.
+        cy.on('window:alert', (str) =>{ expect(str).to.equal('You selected a context menu') })
     })
 })
