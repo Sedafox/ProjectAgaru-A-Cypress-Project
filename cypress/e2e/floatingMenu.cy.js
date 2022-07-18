678af37bf017933c0fc81a5d4e02ee038bbfc816
sedafox <usagimadokii@gmail.com>
2022-07-18 09:17:09 -0500

    Added assertions

--- cypress/e2e/floatingMenu.cy.js
+++ cypress/e2e/floatingMenu.cy.js
@@ -10,19 +10,23 @@ describe('Tests things related to a floating menu', () => {
     it('scrolls the page down, then clicks the home button', () => {
         cy.scrollTo(0,500) //scrolldown some
         floatingMenuPage.floatingHomeButton().click({scrollBehavior: false})
+        cy.url().should('eq', 'http://localhost:7080/floating_menu#home')
     })
 
     it('scrolls the page down, then clicks the News button', () => {
         cy.scrollTo(0,500) //scrolldown some
         floatingMenuPage.floatingNewsButton().click({scrollBehavior: false})
         //We want to tell Cypress not to scroll, this makes it so if the floating menu wasn't on screen,it wouldn't scroll all the way up to click it
+        cy.url().should('eq', 'http://localhost:7080/floating_menu#news')
     })
     it('scrolls the page down, then clicks the Contact button', () => {
         cy.scrollTo(0,500) //scrolldown some
         floatingMenuPage.floatingContactButton().click({scrollBehavior: false})
+        cy.url().should('eq', 'http://localhost:7080/floating_menu#contact')
     })
     it('scrolls the page down, then clicks the About button', () => {
         cy.scrollTo(0,500) //scrolldown some
         floatingMenuPage.floatingAboutButton().click({scrollBehavior: false})
+        cy.url().should('eq', 'http://localhost:7080/floating_menu#about')
     })
 })
